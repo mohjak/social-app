d06f6b4dfef74f59a887b4ebc6c37b7239e077b3
Mohammad Jaqmaqji <mohjak@gmail.com>
2021-09-29 21:01:36 +0300

    Completed register feature.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,5 @@
+const User = require('../models/User')
+
 exports.login = function() {
 
 }
@@ -7,7 +9,13 @@ exports.logout = function() {
 }
 
 exports.register = function(req, res) {
-  res.send("Thanks for trying to register")
+  let user = new User(req.body)
+  user.register()
+  if (user.errors.length) {
+    res.send(user.errors)
+  } else {
+    res.send("Contrats, you have no errors.")
+  }
 }
 
 exports.home = function(req, res) {
---
+++ db.js
@@ -0,0 +1,11 @@
+const dotenv = require("dotenv")
+dotenv.config()
+const mongodb = require('mongodb')
+
+const connectionString = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.pe1k3.mongodb.net/ComplexApp?retryWrites=true&w=majority`
+
+mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
+  module.exports = client.db()
+  const app = require('./app')
+  app.listen(process.env.PORT)
+})
