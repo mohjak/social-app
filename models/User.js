b7504210d27fa50439880f23bb40859993daf274
Mohammad Jaqmaqji <mohjak@gmail.com>
2021-09-30 11:28:12 +0300

    Completed gravatar feature.

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const bcrypt = require("bcryptjs")
 const usersCollection = require("../db").db().collection("users")
 const validator = require("validator")
+const md5 = require('md5')
 
 let User = function(data) {
   this.data = data
@@ -52,6 +53,8 @@ User.prototype.login = function() {
     this.cleanUp()
     usersCollection.findOne({username: this.data.username}).then((attemptedUser) => {
       if (attemptedUser && bcrypt.compareSync(this.data.password, attemptedUser.password)) {
+        this.data = attemptedUser
+        this.getAvatar()
         resolve("Congrats!!!")
       } else {
         reject("Invalid username / password")
@@ -75,11 +78,18 @@ User.prototype.register = function() {
       let salt = bcrypt.genSaltSync(10)
       this.data.password = bcrypt.hashSync(this.data.password, salt)
       await usersCollection.insertOne(this.data)
+      this.getAvatar()
       resolve()
     } else {
       reject(this.errors)
     }
   })
+}
+
+User.prototype.getAvatar = function() {
+  console.log(this.data.email)
+  this.avatar = `https://gravatar.com/avatar/${md5(this.data.email)}?s=128`
+  console.log(this.avatar)
 }
 
 module.exports = User
