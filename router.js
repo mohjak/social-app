b3fbc5def95cee0e47be5a9f7275c840103a698e
Mohammad Jaqmaqji <mohjak@gmail.com>
2021-10-01 12:37:47 +0300

    Finshed saving a follow action into db.

--- router.js
+++ router.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const userController = require('./controllers/userController')
 const postController = require("./controllers/postController")
+const followController = require("./controllers/followController")
 
 // User related routes
 router.get('/', userController.home)
@@ -20,5 +21,8 @@ router.post('/search', postController.search)
 
 // Profile related routes
 router.get('/profile/:username', userController.ifUserExists, userController.profilePostsScreen)
+
+// Follow related routes
+router.post('/addFollow/:username', userController.mustBeLoggedIn, followController.addFollow)
 
 module.exports = router
