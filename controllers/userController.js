1c1dfb75f5491fb3f5bcc87e8f8c1a3372ccef10
Mohammad Jaqmaqji <mohjak@gmail.com>
2021-10-04 15:47:46 +0300

    Finished login through API.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const User = require('../models/User')
 const Post = require('../models/Post')
 const Follow = require('../models/Follow')
+const jwt = require('jsonwebtoken')
 
 exports.doesUsernameExist = function(req, res) {
   User.findByUsername(req.body.username).then(function() {
@@ -22,6 +23,7 @@ exports.sharedProfileData = async function(req, res, next) {
     isVisitorsProfile = req.profileUser._id.equals(req.session.user._id)
     isFollowing = await Follow.isVisitorFollowing(req.profileUser._id, req.visitorId)
   }
+
   req.isVisitorsProfile = isVisitorsProfile
   req.isFollowing = isFollowing
   // retrieve posts, followers, following count
@@ -60,6 +62,15 @@ exports.login = function(req, res) {
     req.session.save(function() {
       res.redirect('/')
     })
+  })
+}
+
+exports.apiLogin = function(req, res) {
+  let user = new User(req.body)
+  user.login().then(function(result) {
+    res.json(jwt.sign({_id: user.data._id}, process.env.JWTSECRET, {expiresIn: '7d'}))
+  }).catch(function(e) {
+    res.json("Sorry, your values are not correct.")
   })
 }
 
